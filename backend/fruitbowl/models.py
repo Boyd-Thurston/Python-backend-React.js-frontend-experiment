b468be2b28819fb6337a10fc992cec64a86e6e71
Boyd_Thurston <boydthurston@gmail.com>
2020-05-25 07:37:39 +1200

    define fruitbowl model

--- backend/fruitbowl/models.py
+++ backend/fruitbowl/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class fruitbowl(models.Model):
+  fruit = models.CharField(max_length=120)
+  url = models.CharField(max_length=250)
+  visable = models.BooleanField(default=False)
+
+  def _str_(self):
+    return self.fruit
