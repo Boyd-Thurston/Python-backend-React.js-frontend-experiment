2691b081b3e81b823d23f100d4ccff26f551b9d6
Boyd_Thurston <boydthurston@gmail.com>
2020-05-25 07:50:00 +1200

    create fruitbowl migration

--- backend/fruitbowl/admin.py
+++ backend/fruitbowl/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import FruitBowl
+
+class FruitBowlAdmin(admin.ModelAdmin):
+  list_display = ('fruit','url', 'visable')
 
 # Register your models here.
+admin.site.register(FruitBowl, FruitBowlAdmin)
--- backend/fruitbowl/models.py
+++ backend/fruitbowl/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class fruitbowl(models.Model):
+class FruitBowl(models.Model):
   fruit = models.CharField(max_length=120)
   url = models.CharField(max_length=250)
   visable = models.BooleanField(default=False)
