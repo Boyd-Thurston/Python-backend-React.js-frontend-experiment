8763904987dc7be63205ce56c2525e1c0ed8a979
Boyd_Thurston <boydthurston@gmail.com>
2020-05-27 13:23:56 +1200

    BUG FIX - pip vs. pip3 install corrected

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,11 +1,11 @@
-    from django.contrib import admin
-    from django.urls import path, include
-    from rest_framework import routers 
-    from fruitbowl import views
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers 
+from fruitbowl import views
 
-    router = routers.DefaultRouter()
-    router.register(r'fruitbowl', views.FruitBowlView, 'fruitbowl')
+router = routers.DefaultRouter()
+router.register(r'fruitbowl', views.FruitBowlView, 'fruitbowl')
 
-    urlpatterns = [
-        path('admin/', admin.site.urls),         path('api/', include(router.urls)) 
-    ]
+urlpatterns = [
+    path('admin/', admin.site.urls),         path('api/', include(router.urls)) 
+]
